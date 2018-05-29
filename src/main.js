5f17935451b99594a581e9d32f3a301a5c210d31
vivty <1979875623@qq.com>
2018-05-29 16:10:27 +0800

    'addheader'

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/styles/reset.css'
+import 'styles/reset.css'
 // 1像素边框
-import './assets/styles/border.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 
